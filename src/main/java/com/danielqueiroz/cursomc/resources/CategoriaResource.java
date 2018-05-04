c6fee652f585d23644e3a6617389bc850579d77f
Daniel Queiroz <danielqueirozak77@gmail.com>
2018-05-03 22:41:00 -0300

    Dominio Categoria adicionado e teste REST OK

---
+++ src/main/java/com/danielqueiroz/cursomc/resources/CategoriaResource.java
@@ -0,0 +1,30 @@
+package com.danielqueiroz.cursomc.resources;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.danielqueiroz.cursomc.domain.Categoria;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResource {
+
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.addAll(Arrays.asList(cat1,cat2));
+		
+		return lista;
+	}
+	
+}
