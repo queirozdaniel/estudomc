f900509df7472ea8d0a1f3831aecde1e0bdce153
Daniel Queiroz <danielqueirozak77@gmail.com>
2018-05-07 23:51:49 -0300

    Atualização das anotações (@JsonIgnore)

--- src/main/java/com/danielqueiroz/cursomc/domain/Cliente.java
+++ src/main/java/com/danielqueiroz/cursomc/domain/Cliente.java
@@ -15,8 +15,7 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
 import com.danielqueiroz.cursomc.domain.enums.TipoCliente;
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Cliente implements Serializable {
@@ -31,7 +30,6 @@ public class Cliente implements Serializable {
 	private String cpfOuCmpj;
 	private Integer tipo;
 
-	@JsonManagedReference
 	@OneToMany(mappedBy="cliente")
 	private List<Endereco> enderecos = new ArrayList<>();
 
@@ -40,7 +38,7 @@ public class Cliente implements Serializable {
 	private Set<String> telefones = new HashSet<>();
 
 	@OneToMany(mappedBy="cliente")
-	@JsonBackReference
+	@JsonIgnore
 	private List<Pedido> pedidos = new ArrayList<>();
 	
 	public Cliente() {
