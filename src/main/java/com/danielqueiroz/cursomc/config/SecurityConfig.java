ad252e035f11b647d37ed25e4169dcf9090cada5
Daniel Queiroz <danielqueirozak77@gmail.com>
2018-06-11 11:41:09 -0300

    Implementando Autorização

--- src/main/java/com/danielqueiroz/cursomc/config/SecurityConfig.java
+++ src/main/java/com/danielqueiroz/cursomc/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.danielqueiroz.cursomc.security.JWTAuthenticationFilter;
+import com.danielqueiroz.cursomc.security.JWTAuthorizationFilter;
 import com.danielqueiroz.cursomc.security.JWTUtil;
 
 @Configuration
@@ -52,6 +53,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.anyRequest().authenticated();
 
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 
