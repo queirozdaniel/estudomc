b699320f0f46dfd91be5e1ce28c451e72368be2e
Daniel Queiroz <danielqueirozak77@gmail.com>
2018-06-12 22:46:42 -0300

    Troca de Senha

--- src/main/java/com/danielqueiroz/cursomc/config/SecurityConfig.java
+++ src/main/java/com/danielqueiroz/cursomc/config/SecurityConfig.java
@@ -39,7 +39,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	private static final String[] PUBLIC_MATCHERS = { "/h2-console/**" };
 
-	private static final String[] PUBLIC_MATCHERS_POST = { "/clientes/**" };
+	private static final String[] PUBLIC_MATCHERS_POST = { "/clientes/**", "/auth/forgot/**" };
 	
 	private static final String[] PUBLIC_MATCHERS_GET = { "/produtos/**", "/categorias/**" };
 
@@ -53,7 +53,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		http.cors().and().csrf().disable();
 
 		http.authorizeRequests().antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
-			.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_POST).permitAll()
+			.antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
 			.anyRequest().authenticated();
 
--- src/main/java/com/danielqueiroz/cursomc/resources/AuthResource.java
+++ src/main/java/com/danielqueiroz/cursomc/resources/AuthResource.java
@@ -4,20 +4,25 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.danielqueiroz.cursomc.dto.EmailDTO;
 import com.danielqueiroz.cursomc.security.JWTUtil;
 import com.danielqueiroz.cursomc.security.UserSS;
+import com.danielqueiroz.cursomc.services.AuthService;
 import com.danielqueiroz.cursomc.services.UserService;
 
 @RestController
-@RequestMapping(value="/auth")
+@RequestMapping(value = "/auth")
 public class AuthResource {
 
 	@Autowired
 	private JWTUtil jwtUtil;
+	@Autowired
+	private AuthService authService;
 	
 	@RequestMapping(value = "/refresh_token", method = RequestMethod.POST)
 	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
@@ -25,6 +30,12 @@ public class AuthResource {
 		String token = jwtUtil.generateToken(user.getUsername());
 		response.addHeader("Authorization", "Bearer " + token);
 		return ResponseEntity.noContent().build();
-}
+	}
 	
+	@RequestMapping(value = "/forgot", method = RequestMethod.POST)
+	public ResponseEntity<Void> forgot(@RequestBody EmailDTO emailDto) {
+		authService.sendNewPassword(emailDto.getEmail());
+		return ResponseEntity.noContent().build();
+	}
+
 }
