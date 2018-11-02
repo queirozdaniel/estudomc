083899424dc5b8d19f7b4ce904d1b6cf6da79c0c
Daniel Queiroz <danielqueirozak77@gmail.com>
2018-11-02 17:13:07 -0300

    Configurando e salvando arquivo no S3

--- src/main/java/com/danielqueiroz/cursomc/CursoMcApplication.java
+++ src/main/java/com/danielqueiroz/cursomc/CursoMcApplication.java
@@ -1,12 +1,17 @@
 package com.danielqueiroz.cursomc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.danielqueiroz.cursomc.services.S3Service;
+
 @SpringBootApplication
 public class CursoMcApplication implements CommandLineRunner{
 
+	@Autowired
+	private S3Service s3Service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoMcApplication.class, args);
@@ -14,6 +19,6 @@ public class CursoMcApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-
+		s3Service.uploadFile("/home/daniel/Downloads/Atlas.jpg");
 	}
 }
