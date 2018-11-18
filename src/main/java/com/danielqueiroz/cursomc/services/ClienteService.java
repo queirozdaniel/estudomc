1f9137d543463474516e774f6e2320a7aebee748
Daniel Queiroz <danielqueirozak77@gmail.com>
2018-11-17 21:01:49 -0300

    Ajustando imagem e expondo o header Authorization

--- src/main/java/com/danielqueiroz/cursomc/services/ClienteService.java
+++ src/main/java/com/danielqueiroz/cursomc/services/ClienteService.java
@@ -51,6 +51,9 @@ public class ClienteService {
 	@Value("${img.prefix.client.profile}")
 	private String prefix;
 	
+	@Value("${img.profile.size}")
+	private Integer size;
+	
 	public Cliente find(Integer id) {
 		
 		UserSS user = UserService.authenticated();
@@ -135,6 +138,9 @@ public class ClienteService {
 		}
 		
 		BufferedImage imageJpg = imageService.getJpgImageFromFile(multipartFile);
+		imageJpg = imageService.cropSquare(imageJpg);
+		imageJpg = imageService.resize(imageJpg, size);
+		
 		String fileName = prefix + user.getId() + ".jpg";
 		
 		return s3Service.uploadFile(imageService.getInputStream(imageJpg, "jpg"), fileName, "image");
