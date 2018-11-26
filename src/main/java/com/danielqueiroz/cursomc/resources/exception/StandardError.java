c9e8e51a28a788b7dc5abab33d32fe88037e4cec
Daniel Queiroz <danielqueirozak77@gmail.com>
2018-11-25 21:41:27 -0300

    Padrinizando formato de excecoes

--- src/main/java/com/danielqueiroz/cursomc/resources/exception/StandardError.java
+++ src/main/java/com/danielqueiroz/cursomc/resources/exception/StandardError.java
@@ -2,18 +2,29 @@ package com.danielqueiroz.cursomc.resources.exception;
 
 import java.io.Serializable;
 
-public class StandardError implements Serializable{
+public class StandardError implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
-	private Integer status;
-	private String msg;
 	private Long timeStamp;
+	private Integer status;
+	private String error;
+	private String message;
+	private String path;
 
-	public StandardError(Integer status, String msg, Long timeStamp) {
-		super();
-		this.status = status;
-		this.msg = msg;
+	public StandardError(Long t, Integer s, String e, String m, String p) {
+		this.timeStamp = t;
+		this.status = s;
+		this.error = e;
+		this.message = m;
+		this.path = p;
+	}
+
+	public Long getTimeStamp() {
+		return timeStamp;
+	}
+
+	public void setTimeStamp(Long timeStamp) {
 		this.timeStamp = timeStamp;
 	}
 
@@ -25,20 +36,28 @@ public class StandardError implements Serializable{
 		this.status = status;
 	}
 
-	public String getMsg() {
-		return msg;
+	public String getError() {
+		return error;
 	}
 
-	public void setMsg(String msg) {
-		this.msg = msg;
+	public void setError(String error) {
+		this.error = error;
 	}
 
-	public Long getTimeStamp() {
-		return timeStamp;
+	public String getMessage() {
+		return message;
 	}
 
-	public void setTimeStamp(Long timeStamp) {
-		this.timeStamp = timeStamp;
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
 	}
 
 }
--- src/main/java/com/danielqueiroz/cursomc/resources/exception/ValidationError.java
+++ src/main/java/com/danielqueiroz/cursomc/resources/exception/ValidationError.java
@@ -7,9 +7,9 @@ public class ValidationError extends StandardError {
 	private static final long serialVersionUID = 1L;
 
 	private List<FieldMessage> errors = new ArrayList<>();
-	
-	public ValidationError(Integer status, String msg, Long timeStamp) {
-		super(status, msg, timeStamp);
+
+	public ValidationError(Long t, Integer s, String e, String m, String p) {
+		super(t, s, e, m, p);
 	}
 
 	public List<FieldMessage> getErrors() {
